c48026d6cc017344bf6f682ba44bd8b54b001084
Elisa Vaccaro <erv230@nyu.edu>
2017-10-04 03:38:43 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,15 +6,15 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, val|
     holder.push(val)
   end
-  holder.each do |a,b|
-    while holder.length > 1
-    if a < b
-      holder.delete(b)
-    elsif b < a
-      holder.delete(a)
-    end
-  end
-  holder
-    end
-    name_hash.key(holder[0])
+  i = 0 
+while holder.length > 1
+if holder[i] > holder[i+1]
+  holder.delete(holder[i])
+  elsif holder[i] < holder[i+1]
+  holder.delete(holder[i+1])
 end
+end
+holder
+name_hash.key(holder[0])
+end
+
